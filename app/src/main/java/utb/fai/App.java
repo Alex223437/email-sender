11179e9ff448a782131b97b83cee9f4d2fe1e7df
Alex223437 <alex223437@gmail.com>
2024-09-30 18:37:29 +0200

    uploading solution

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -3,11 +3,23 @@ package utb.fai;
 public class App {
 
     public static void main(String[] args) {
-        // TODO: Implement input parameter processing
-        
+        // checking arguments
+        if (args.length < 5) {
+            System.out.println("Usage: java App <smtp_host> <port> <from_email> <to_email> <subject> <message>");
+            return;
+        }
+
+        String smtpHost = args[0];
+        int port = Integer.parseInt(args[1]);
+        String fromEmail = args[2];
+        String toEmail = args[3];
+        String subject = args[4];
+        String message = args[5];
+
+        // send email
         try {
-            EmailSender sender = new EmailSender("smtp.utb.cz", 25);
-            sender.send("you@utb.cz", "you@utb.cz", "Email from Java", "Funguje to?\nSnad...");
+            EmailSender sender = new EmailSender(smtpHost, port);
+            sender.send(fromEmail, toEmail, subject, message);
             sender.close();
         } catch (Exception e) {
             e.printStackTrace();
--- app/src/main/java/utb/fai/EmailSender.java
+++ app/src/main/java/utb/fai/EmailSender.java
@@ -1,32 +1,76 @@
 package utb.fai;
 
-import java.net.*;
-import java.io.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
 
 public class EmailSender {
-    /*
-     * Constructor opens Socket to host/port. If the Socket throws an exception
-     * during opening,
-     * the exception is not handled in the constructor.
-     */
-    public EmailSender(String host, int port) throws UnknownHostException, IOException {
+    private Socket socket;
+    private InputStream input;
+    private OutputStream output;
 
+    //connection to the socjket
+    public EmailSender(String host, int port) throws UnknownHostException, IOException {
+        socket = new Socket(host, port);
+        input = socket.getInputStream();
+        output = socket.getOutputStream();
+        System.out.println("Connected to server");
     }
 
-    /*
-     * Sends email from an email address to an email address with some subject and
-     * text.
-     * If the Socket throws an exception during sending, the exception is not
-     * handled by this method.
-     */
+    
     public void send(String from, String to, String subject, String text) throws IOException {
 
+        String message = "EHLO " + InetAddress.getLocalHost().getHostName() + "\r\n";
+        output.write(message.getBytes());
+        output.flush();
+        readResponse();
+
+
+        message = "MAIL FROM:<" + from + ">\r\n";
+        output.write(message.getBytes());
+        output.flush();
+        readResponse();
+
+        message = "RCPT TO:<" + to + ">\r\n";
+        output.write(message.getBytes());
+        output.flush();
+        readResponse();
+
+        message = "DATA\r\n";
+        output.write(message.getBytes());
+        output.flush();
+        readResponse();
+
+        message = "Subject: " + subject + "\r\n\r\n" + text + "\r\n.\r\n";
+        output.write(message.getBytes());
+        output.flush();
+        readResponse();
+
+        message = "QUIT\r\n";
+        output.write(message.getBytes());
+        output.flush();
+        readResponse();
+    }
+
+    private void readResponse() throws IOException {
+        byte[] response = new byte[1024];
+        int len = input.read(response);
+        System.out.write(response, 0, len);
+        System.out.flush();
     }
 
     /*
      * Sends QUIT and closes the socket
      */
-    public void close() {
-
+    public void close() throws IOException {
+        if (socket != null && !socket.isClosed()) {
+            socket.close();
+        }
     }
 }
