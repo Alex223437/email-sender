75dbc86acb66652fde6a6a3cec68f5ff8d9abcda
Alex223437 <alex223437@gmail.com>
2024-09-30 18:57:34 +0200

    gitignore updates

--- app/src/main/java/utb/fai/EmailSender.java
+++ app/src/main/java/utb/fai/EmailSender.java
@@ -1,12 +1,8 @@
 package utb.fai;
 
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 
 
 
